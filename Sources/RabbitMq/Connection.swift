a8b9b07565028a8b584209d960697de0e83af088
Jesse L. Zamora <xtremekforever@gmail.com>
2024-05-17 06:54:23 -0400

    Add TLS config + isConnected to Connection

--- Sources/RabbitMq/Connection.swift
+++ Sources/RabbitMq/Connection.swift
@@ -1,6 +1,7 @@
 import AMQPClient
 import Foundation
 import NIO
+import NIOSSL
 
 public actor Connection {
     private let config: AMQPConnectionConfiguration
@@ -11,12 +12,18 @@ public actor Connection {
 
     public init(
         eventLoop: EventLoop = MultiThreadedEventLoopGroup(numberOfThreads: 1).next(),
-        _ url: String = ""
+        _ url: String = "",
+        tls: TLSConfiguration = TLSConfiguration.makeClientConfiguration()
     ) throws {
         self.eventLoop = eventLoop
-        self.config = try AMQPConnectionConfiguration.init(url: url)
+        self.config = try AMQPConnectionConfiguration.init(url: url, tls: tls)
+    }
 
-        // TODO: Support TLS configuration
+    public func isConnected() -> Bool {
+        if let conn = self.connection {
+            return conn.isConnected
+        }
+        return false
     }
 
     public func reuseChannel() async throws -> AMQPChannel {
