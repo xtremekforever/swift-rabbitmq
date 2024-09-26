f5dfb66fcfd177a26be160ac1a6e5795d9945f14
Jesse L. Zamora <xtremekforever@gmail.com>
2024-09-26 10:07:20 -0400

    Stop using cancelOnGracefulShutdown which currently leaks
     - Use cancelWhenGracefulShutdown paired with Task.sleep instead.

--- Sources/RabbitMq/Utils/gracefulCancellableDelay.swift
+++ Sources/RabbitMq/Utils/gracefulCancellableDelay.swift
@@ -1,11 +1,16 @@
 import AsyncAlgorithms
+import ServiceLifecycle
 
 /// Delay the current task supporting graceful shutdown.
 ///
 /// - Parameters:
 ///   - timeout: The Duration to delay the current task
 public func gracefulCancellableDelay(_ timeout: Duration) async {
-    for await _ in AsyncTimerSequence(interval: timeout, clock: .continuous).cancelOnGracefulShutdown() {
-        break
+    await cancelWhenGracefulShutdown {
+        do {
+            try await Task.sleep(for: timeout)
+        } catch {
+            // Ignore errors
+        }
     }
 }
