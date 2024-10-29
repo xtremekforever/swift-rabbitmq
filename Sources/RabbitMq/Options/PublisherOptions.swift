6bd1b5722ff53a4265a238502a3f87099dab3d79
Jesse L. Zamora <xtremekforever@gmail.com>
2024-10-29 16:26:35 -0400

    Add documentation for Publisher and related structures

--- Sources/RabbitMq/Options/PublisherOptions.swift
+++ Sources/RabbitMq/Options/PublisherOptions.swift
@@ -1,10 +1,20 @@
 import AMQPProtocol
 
+/// Definition of available options for publishing a message to RabbitMq.
 public struct PublisherOptions: Sendable {
     var mandatory: Bool
     var immediate: Bool
     var properties: Properties
 
+    /// Create the publisher options.
+    ///
+    /// For more information on the options used, see the [basicPublish](https://github.com/funcmike/rabbitmq-nio/blob/cb9c294fda00f57db116abd297df21d078b5d027/Sources/AMQPClient/AMQPChannel.swift#L62-L119)
+    /// function reference which is the underlying implementation used for publishing.
+    ///
+    /// - Parameters:
+    ///   - mandatory: If set to true, message must be able to be delivered to a queue or it will be returned.
+    ///   - immediate: If set to true, message must be delivered to a consumer immediately or it will be returned.
+    ///   - properties: Extra properties to define for this publisher (see [Properties.swift](https://github.com/funcmike/rabbitmq-nio/blob/main/Sources/AMQPProtocol/Properties.swift) in `rabbitmq-nio`)
     public init(
         mandatory: Bool = false,
         immediate: Bool = false,
--- Sources/RabbitMq/Publisher/Publisher.swift
+++ Sources/RabbitMq/Publisher/Publisher.swift
@@ -1,11 +1,20 @@
 import AMQPClient
 import Logging
+import NIOCore
 
+/// Structure for a RabbitMq "publisher", which sends an AMQP message on an exchange.
 public struct Publisher: Sendable {
     let connection: Connection
     let configuration: PublisherConfiguration
     let logger: Logger
 
+    /// Create the publisher. If the default parameters are used, this publishes to the default exchange on the broker.
+    ///
+    /// - Parameters:
+    ///   - connection: The connection for this publisher to use.
+    ///   - exchangeName: The name of the exchange to publish on. This can be empty to publish on the default exchange.
+    ///   - exchangeOptions: The options for declaring the exchange. This is only used if the `exchangeName` is set.
+    ///   - publisherOptions: The options for publishing the message. This is always used.
     public init(
         _ connection: Connection,
         _ exchangeName: String = "",
@@ -21,14 +30,54 @@ public struct Publisher: Sendable {
         self.logger = connection.logger
     }
 
+    /// Publish a message to the broker (no retries).
+    ///
+    /// - Parameters:
+    ///   - data: The string data of the message to publish. This string will be encoded to UTF-8 before sending.
+    ///   - routingKey: The optional routing key to use for publishing the message.
+    /// - Throws: `AMQPConnectionError.connectionClosed` if the connection to the broker is not open,
+    ///         or an `NIO` or `AMQPClient` error.
     public func publish(_ data: String, routingKey: String = "") async throws {
-        try await connection.performPublish(configuration, data, routingKey: routingKey)
+        try await connection.performPublish(configuration, ByteBuffer(string: data), routingKey: routingKey)
     }
 
-    public func retryingPublish(_ data: String, routingKey: String = "", retryInterval: Duration = .seconds(30))
-        async throws
-    {
+    /// Publish a message to the broker with retries.
+    ///
+    /// This method will not return until it is cancelled or is able to publish the message.
+    /// It performs a publish operation but will retry indefinitely until it is able to
+    /// publish the message to the broker.
+    ///
+    /// - Parameters:
+    ///   - data: The NIO `ByteBuffer` data of the message to publish. This can be JSON, XML, or binary data.
+    ///   - routingKey: The optional routing key to use for publishing the message.
+    ///   - retryInterval: The interval at which to retry publishing the message.
+    /// - Throws: `AMQPConnectionError.connectionClosed` if the connection to the broker is not open,
+    ///         or an `NIO` or `AMQPClient` error.
+    public func retryingPublish(
+        _ data: ByteBuffer, routingKey: String = "", retryInterval: Duration = .seconds(30)
+    ) async throws {
         return try await RetryingPublisher(connection, configuration, retryInterval).publish(
-            data, routingKey: routingKey)
+            data, routingKey: routingKey
+        )
+    }
+
+    /// Publish a message to the broker with retries.
+    ///
+    /// This method will not return until it is cancelled or is able to publish the message.
+    /// It performs a publish operation but will retry indefinitely until it is able to
+    /// publish the message to the broker.
+    ///
+    /// - Parameters:
+    ///   - data: The string data of the message to publish. This string will be encoded to UTF-8 before sending.
+    ///   - routingKey: The optional routing key to use for publishing the message.
+    ///   - retryInterval: The interval at which to retry publishing the message.
+    /// - Throws: `AMQPConnectionError.connectionClosed` if the connection to the broker is not open,
+    ///         or an `NIO` or `AMQPClient` error.
+    public func retryingPublish(
+        _ data: String, routingKey: String = "", retryInterval: Duration = .seconds(30)
+    ) async throws {
+        return try await RetryingPublisher(connection, configuration, retryInterval).publish(
+            ByteBuffer(string: data), routingKey: routingKey
+        )
     }
 }
