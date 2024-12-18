f02219c8a02f415bf0c136bcbd9de02e33159f93
Jesse L. Zamora <xtremekforever@gmail.com>
2024-12-18 14:07:36 -0500

    Enable minimum iOS, tvOS, and watchOS versions for project

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "swift-rabbitmq",
     platforms: [
-        .macOS(.v14)
+        .iOS(.v13),
+        .macOS(.v14),
+        .tvOS(.v13),
+        .watchOS(.v6),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
